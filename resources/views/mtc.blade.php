6b1e42a9e76f7129d9e8c797ed176e6b5d75b6b4
ahmadrifai <ahmatrifai8721@gmail.com>
2025-01-01 18:18:41 +0700

    update miantenance page

---
+++ resources/views/mtc.blade.php
@@ -0,0 +1,53 @@
+<!doctype html>
+<html>
+
+<head>
+    <title>Site Maintenance</title>
+    <meta charset="utf-8" />
+    <meta name="robots" content="noindex" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <style>
+        body {
+            text-align: center;
+            padding: 150px;
+        }
+
+        h1 {
+            font-size: 50px;
+        }
+
+        body {
+            font: 20px Helvetica, sans-serif;
+            color: #333;
+        }
+
+        article {
+            display: block;
+            text-align: left;
+            width: 650px;
+            margin: 0 auto;
+        }
+
+        a {
+            color: #dc8100;
+            text-decoration: none;
+        }
+
+        a:hover {
+            color: #333;
+            text-decoration: none;
+        }
+    </style>
+</head>
+
+<body>
+    <article>
+        <h1>Kami Segera Kembali</h1>
+        <div>
+            <p>Maaf Atas Ketidak Nyamanannya Saat ini kami sedang melakukan peningkatan system</p>
+            <p>&mdash; Tim IT</p>
+        </div>
+    </article>
+</body>
+
+</html>
